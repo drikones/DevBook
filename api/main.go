d21f61293c45dcd603a15cdccd8809c6ac551677
drikones <adriano_rocha15@hotmail.com>
2026-08-23 12:19:55 -0300

    Pacote config

--- api/main.go
+++ api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"DevBook/src/config"
 	"DevBook/src/router"
 	"fmt"
 	"log"
@@ -10,6 +11,9 @@ import (
 // TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 func main() {
+	config.Carregar()
+	fmt.Println(config.Porta)
+	fmt.Println(config.StringConexaoBanco)
 	fmt.Println("Rodando API")
 	r := router.Gerar()
 	log.Fatal(http.ListenAndServe(":8000", r))
