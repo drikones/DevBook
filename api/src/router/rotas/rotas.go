82c77085eca870c939e56282a7dee1e0d67b7edc
drikones <adriano_rocha15@hotmail.com>
2026-08-23 11:35:15 -0300

    Inserindo rotas dentro do router

--- api/src/router/rotas/rotas.go
+++ api/src/router/rotas/rotas.go
@@ -1,6 +1,10 @@
 package rotas
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 // Rota representa todas as rotas na API
 type Rota struct {
@@ -8,4 +12,12 @@ type Rota struct {
 	Metodo             string
 	Funcao             func(http.ResponseWriter, *http.Request)
 	RequerAutenticacao bool
+}
+
+func Configurar(r *mux.Router) *mux.Router {
+	rotas := rotasUsuarios
+	for _, rota := range rotas {
+		r.HandleFunc(rota.Uri, rota.Funcao).Methods(rota.Metodo)
+	}
+	return r
 }
