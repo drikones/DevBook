ba88179a8f65cd5cbba816923f944bc1161cdeeb
drikones <adriano_rocha15@hotmail.com>
2026-08-30 11:28:08 -0300

    Busca de usuário por ID

--- api/src/repositorios/usuarios.go
+++ api/src/repositorios/usuarios.go
@@ -61,3 +61,25 @@ func (repositorio Usuarios) Buscar(nomeOuNick string) ([]modelos.Usuario, error)
 	}
 	return usuarios, nil
 }
+
+// BuscarPorID trás um usuário pelo seu ID
+func (repositorio Usuarios) BuscarPorID(ID uint64) (modelos.Usuario, error) {
+	linhas, erro := repositorio.db.Query(
+		"SELECT id, nome, nick, email, criadoEm from usuarios where id =?", ID)
+	if erro != nil {
+		return modelos.Usuario{}, erro
+	}
+	defer linhas.Close()
+	var usuario modelos.Usuario
+	if linhas.Next() {
+		if erro = linhas.Scan(
+			&usuario.ID,
+			&usuario.Nome,
+			&usuario.Nick,
+			&usuario.Email,
+			&usuario.CriadoEm); erro != nil {
+			return modelos.Usuario{}, erro
+		}
+	}
+	return usuario, nil
+}
