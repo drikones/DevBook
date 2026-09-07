fc490051adc3e4fa53aa44ac1651ec3350821ec6
drikones <adriano_rocha15@hotmail.com>
2026-09-06 23:42:49 -0300

    Modelo de publicações

--- sql/sql.sql
+++ sql/sql.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE IF NOT EXISTS devbook;
 USE devbook;
 
+DROP TABLE IF EXISTS publicacoes;
 DROP TABLE IF EXISTS usuarios;
 DROP TABLE IF EXISTS seguidores;
 
@@ -19,4 +20,14 @@ CREATE TABLE seguidores (
     FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE,
     FOREIGN KEY (seguidor_id) REFERENCES usuarios(id) ON DELETE CASCADE,
     PRIMARY KEY (usuario_id,seguidor_id)
+) ENGINE=INNODB;
+
+CREATE TABLE publicacoes (
+    id int auto_increment primary key,
+    titulo varchar(50) not null,
+    conteudo varchar(300) not null,
+    autor_id int not null,
+    curtidas int default 0,
+    criadaEm timestamp default current_timestamp,
+    FOREIGN KEY (autor_id) REFERENCES usuarios(id) ON DELETE CASCADE
 ) ENGINE=INNODB;
