782a714b819d4e88c4719af535e50a5ea5a71d60
drikones <adriano_rocha15@hotmail.com>
2026-09-07 13:37:31 -0300

    Métodos para validação da publicação

--- api/src/modelos/publicacao.go
+++ api/src/modelos/publicacao.go
@@ -1,6 +1,10 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // Publicacao representa uma publicação feita por um usuário.
 type Publicacao struct {
@@ -11,4 +15,30 @@ type Publicacao struct {
 	AutorNick uint64    `json:"autorNick,omitempty"`
 	Curtidas  uint64    `json:"curtidas"`
 	CriadaEm  time.Time `json:"criadaEm,omitempty"`
+}
+
+// Prepara faz as validações iniciais da publicação.
+func (publicacao *Publicacao) Preparar() error {
+	if erro := publicacao.validar(); erro != nil {
+		return erro
+	}
+	publicacao.formatar()
+	return nil
+}
+
+// validar se os campos obrigatórios estão preenchidos.
+func (publicacao *Publicacao) validar() error {
+	if publicacao.Titulo == "" {
+		return errors.New("titulo é obrigatório e não pode estar em branco")
+	}
+	if publicacao.Conteudo == "" {
+		return errors.New("conteudo é obrigatório e não pode estar em branco")
+	}
+	return nil
+}
+
+// formatar faz o trim dos campos e remove espaços desnecessários.
+func (publicacao *Publicacao) formatar() {
+	publicacao.Titulo = strings.TrimSpace(publicacao.Titulo)
+	publicacao.Conteudo = strings.TrimSpace(publicacao.Conteudo)
 }
